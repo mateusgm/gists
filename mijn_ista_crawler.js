4a2072472890e19600e372e679197aea5766e2ca
unknown <mateusgm0@gmail.com>
2023-11-29 09:03:55 +0100

    mijn ista crawler

---
+++ mijn_ista_crawler.js
@@ -0,0 +1,71 @@
+
+$.postJSON = function(url, data, callback) {
+    return jQuery.ajax({
+    headers: { 
+        'Accept': 'application/json',
+        'Content-Type': 'application/json' 
+    },
+    'type': 'POST',
+    'url': url,
+    'data': JSON.stringify(data),
+    'dataType': 'json',
+    'success': callback
+    });
+};
+
+Date.prototype.ftime = function() {
+	return this.toISOString().slice(0,19);
+}
+
+Date.prototype.fdate = function() {
+	return this.toISOString().slice(0,10);
+}
+
+Date.prototype.add = function(value) {
+    var date = new Date(this.valueOf());
+    date.setMonth(date.getMonth() + (value.months || 0));
+    date.setDate(date.getDate() + (value.days || 0));
+    return date;
+};
+
+
+
+function crawl(start, stop, interval) {
+	var end;
+
+	if(interval == 'month') {
+		start = start.add({ months: 1 });
+		end   = start.add({ months: 1, days: -1 });
+	}
+	if(interval == 'week') {
+		start = start.add({ days: 7 });
+		end   = start.add({ days: 6 });
+	}
+
+	var params = {
+		"JWT": JWT,
+		"LANG": "en-US",
+		"Cuid": "f8842752-89d7-4801-95ee-2994feaacf14",
+		"Billingperiod":  {
+			y: start.add({ months: 6 }).getFullYear() +'',
+			s: start.ftime(),
+			e: end.ftime(),
+		}
+	}
+
+	$.postJSON(url, params, function(data) { 
+		console.log(start.fdate(), data.ServicesComp[0].TotalNow);
+		results[start.fdate()] = data.ServicesComp[0].TotalNow;
+
+		if(start < stop) crawl(start, stop, interval);
+	})
+}
+
+var url = 'https://mijn.ista.nl/api/Values/ConsumptionValues';
+
+var results = {},
+	start = new Date(2023,10,26,2).add({ days: -7*3 }),
+	stop = new Date(),
+    JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJZanhFaGNLdTVmeDhjTEh6SkVkclY3dUdEUlpMTStEZjhvWVB0QlpqdkNVL0NmL2lyRERuKzRQYzRPMlhZOE1iIiwibmJmIjoxNzAxMjQzNTIwLCJleHAiOjE3MDEyNDQ3MjAsImlhdCI6MTcwMTI0MzUyMCwiaXNzIjoiaHR0cHM6Ly9taWpuLmlzdGEubmwifQ.H-7yUUUWRIXYikVUwRo5S8WCL2QpWq0RI52GcrFEQrE';
+
+crawl(start, stop, 'week')
