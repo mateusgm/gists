45ee8fc74bd788822e2d95e9c789c35bdd0d88f7
unknown <mateusgm0@gmail.com>
2023-12-01 12:10:57 +0100

    mijn ista crawler

--- mijn_ista_crawler.js
+++ mijn_ista_crawler.js
@@ -1,24 +1,24 @@
 
 $.postJSON = function(url, data, callback) {
     return jQuery.ajax({
-    headers: { 
-        'Accept': 'application/json',
-        'Content-Type': 'application/json' 
-    },
-    'type': 'POST',
-    'url': url,
-    'data': JSON.stringify(data),
-    'dataType': 'json',
-    'success': callback
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        'type': 'POST',
+        'url': url,
+        'data': JSON.stringify(data),
+        'dataType': 'json',
+        'success': callback
     });
 };
 
 Date.prototype.ftime = function() {
-	return this.toISOString().slice(0,19);
+    return this.toISOString().slice(0,19);
 }
 
 Date.prototype.fdate = function() {
-	return this.toISOString().slice(0,10);
+    return this.toISOString().slice(0,10);
 }
 
 Date.prototype.add = function(value) {
@@ -28,40 +28,41 @@ Date.prototype.add = function(value) {
     return date;
 };
 
+function crawl(start, end, stop, callback) {
+    if(start >= stop)
+        return;
 
-function crawl(start, stop, interval) {
-	var end;
+    var url = 'https://mijn.ista.nl/api/Values/ConsumptionValues';
 
-	if(interval == 'month') {
-		start = start.add({ months: 1 });
-		end   = start.add({ months: 1, days: -1 });
-	}
-	if(interval == 'week') {
-		start = start.add({ days: 7 });
-		end   = start.add({ days: 6 });
-	}
+    var params = {
+        "JWT": JSON.parse(sessionStorage.uBase).JWT,
+        "Cuid": JSON.parse(sessionStorage.uBase).Cus[0].Cuid,
+        "LANG": "en-US",
+        "Billingperiod":  {
+            y: start.add({ months: 6 }).getFullYear() +'',
+            s: start.ftime(),
+            e: end > stop ? stop.ftime() : end.ftime(),
+        }
+    }
 
-	var params = {
-		"JWT": JSON.parse(sessionStorage.uBase).JWT,
-		"Cuid": JSON.parse(sessionStorage.uBase).Cus[0].Cuid,
-		"LANG": "en-US",
-		"Billingperiod":  {
-			y: start.add({ months: 6 }).getFullYear() +'',
-			s: start.ftime(),
-			e: end.ftime(),
-		}
-	}
-
-	$.postJSON(url, params, function(data) { 
-		console.log(start.fdate(), data.ServicesComp[0].TotalNow);
-		if(start < stop)
-			crawl(start, stop, interval);
-	})
+    $.postJSON(url, params, function(data) {
+        console.log(start.fdate(), data.ServicesComp[0].TotalNow);
+        callback(start, stop);
+    })
 }
 
-var url = 'https://mijn.ista.nl/api/Values/ConsumptionValues';
+function weekly(start, stop) {
+    var start = start.add({ days: 7 }),
+        end   = start.add({ days: 6 });
+    crawl(start, end, stop, weekly);
+}
 
-var start = new Date(2023,10,26,2).add({ days: -7*3 }),
-	stop = new Date();
+function monthly(start, stop) {
+    var    start = start.add({ months: 1 });
+        end   = start.add({ months: 1, days: -1 });
+    crawl(start, end, stop, monthly);
+}
 
-crawl(start, stop, 'week')
+var stop = new Date().add({ days: -1 });
+weekly( new Date(2023,10,5,2), stop);
+// monthly( new Date(2023,9,1,2) , stop);
